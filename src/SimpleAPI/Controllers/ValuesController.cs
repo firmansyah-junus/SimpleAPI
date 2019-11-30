d0b39e6ee021c557d7f22d585a5a6f55a35cc543
firmansyah-junus <firmansyah.junus@yahoo.com>
2019-11-30 19:01:49 +0700

    Fixed unit test :)

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "tester";
+            return "tester test";
         }
 
         // POST api/values
