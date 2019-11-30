e2d57e3e46f614aa93fb8f2a8890980b28941e17
firmansyah-junus <firmansyah.junus@yahoo.com>
2019-11-30 18:52:45 +0700

    Broken unit test :(

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "tester test";
+            return "tester";
         }
 
         // POST api/values
